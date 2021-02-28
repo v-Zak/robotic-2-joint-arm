1413fac04e8502e0cdd686d11b9f845b09055fb9
v-Zak <v-Zak@users.noreply.github.com>
2021-02-28 18:36:22 +0000

    added base point

--- main.py
+++ main.py
@@ -16,16 +16,17 @@ class segment:
         direction = self.target_position.sub(self.a_position)
         self.angle = math.atan2(direction.y, direction.x)        
         direction.set_mag(self.length)
-        print (direction.get_mag())
         direction.x *= -1
         direction.y *= -1
         self.a_position = self.target_position.add(direction)
-        self.calculate_b()
-        self.show()
+        
     
     def calculate_b(self):
         self.b_position.x = self.a_position.x + self.length * math.cos(self.angle)
         self.b_position.y = self.a_position.y + self.length * math.sin(self.angle)
+    
+    def fix_to_base(self,distance_to_base):
+        self.a_position = self.a_position.add(distance_to_base)
         
     def show(self):        
         self.canvas.coords(self.line, self.a_position.x, self.a_position.y, self.b_position.x, self.b_position.y)        
@@ -62,6 +63,7 @@ class position:
 
 height = 400
 width = 400
+base_position = position(50,200)
 framerate = 48 #fps
 #timestep in ms based on framerate
 timestep = int(1000*(1/framerate))
@@ -85,7 +87,18 @@ def main():
     segment1.update()   
 
     segment2.target_position = segment1.a_position    
-    segment2.update()   
+    segment2.update() 
+
+    distance_to_base = base_position.sub(segment2.a_position)
+
+    segment1.fix_to_base(distance_to_base)
+    segment2.fix_to_base(distance_to_base)  
+
+    segment1.calculate_b()
+    segment2.calculate_b()
+
+    segment1.show()
+    segment2.show()
 
     canvas.after(timestep, main)
 
