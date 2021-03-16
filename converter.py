73f5fded05bfcad1d6ed931e6a4077a7247ebbdb
v-Zak <v-Zak@users.noreply.github.com>
2021-03-16 17:03:29 +0000

    started conversion to PWM need to import pi module still

--- converter.py
+++ converter.py
@@ -1,17 +1,42 @@
 "41.269250430844856 71.82374207264324 | 42.980050940806315 72.99282382825892 |"
 
+import time
+
 with open("output.txt","r") as r:
     tasks = r.readlines()
 
-temp2 = [] 
+temp3 = [] 
 for task in tasks:  
-    temp = []  
+    temp2 = []  
     task = task.split(" | ")
     task.pop(-1)
     for angles in task:
+        temp = []
         angles = angles.split()
         for angle in angles:
             angle = float(angle)
-        temp.append(angles)
-    temp2.append(temp)
-tasks = temp2
+            temp.append(angle)
+        temp2.append(temp)
+    temp3.append(temp2)
+tasks = temp3
+
+def update_servo(angle,real_servo):
+    pwm = angle/18 + 2.5
+    real_servo.ChangeDutyCycle(pwm)    
+
+def move_servos(servo_angles, *args):
+    timestep = 1/24
+    if args:
+        timestep = args[0]
+    #only considering 2 servos
+    real_servos = [10,11]
+    for servos in servo_angles:     
+        i = 0
+        for servo in servos:
+            update_servo(servo,real_servos[i])
+            i += 1
+        time.sleep(timestep)
+            
+
+
+move_servos(tasks[0])
